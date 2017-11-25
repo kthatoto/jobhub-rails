1debeec977d66aa4f7aace1db485a87754ca4966
tk1to <tk02dr@gmail.com>
2017-11-26 05:23:48 +0900

    top

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,9 +2,9 @@ class TopController < ApplicationController
 
 
   def dashboard
-    @issues = []
+    issues = []
     Issue.order("id desc").limit(10).each do |issue|
-      @issues << {
+      issues << {
         id: issues.id,
         title: issue.title,
         price: issue.price,
@@ -19,8 +19,43 @@ class TopController < ApplicationController
         name: @user.name,
         detail: @user.detail
       },
-      issues: @issues,
-      skills: Skill.all
+      issues: issues,
+      skills: Skill.pluck(:name)
+    }
+  end
+
+  def mypage
+
+    challenges = []
+    @user.challenges.order("id desc").limit(10).each do |challenge|
+      challenges << {
+        title: challenge.title,
+        cost: challenge.cost,
+        status: challenge.status,
+        issue: {
+          id: challenge.issue.id,
+          owner_name: challenge.issue.user.name,
+        },
+      }
+    end
+
+    issues = []
+    @user.issues.order("id desc").limit(10).each do |issue|
+      issues << {
+        id: issue.id,
+        title: issue.title,
+        cost: issue.cost,
+        status: issue.is_opend ? "open" : "closed",
+      }
+    end
+
+    render json: {
+      user: {
+        name: @user.name,
+        detail: @user.detail
+      },
+      challenges: challenges,
+      issues: issues,
     }
   end
 end
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,8 +3,7 @@ class Challenge < ApplicationRecord
   enum status: {
     challenging: 0,
     judging: 1,
-    rejected: 2,
-    accepted: 3
+    accepted: 2,
   }
   belongs_to :issue
   belongs_to :user
