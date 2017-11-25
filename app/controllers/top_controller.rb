c555220020893c1be0945ccb1cca555295804d99
tk1to <tk02dr@gmail.com>
2017-11-26 01:34:05 +0900

    pg

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,18 @@
 class TopController < ApplicationController
 
   def dashboard
-    @issues = Issue.order("id desc").limit(10)
+    @issues = []
+    Issue.order("id desc").limit(10).each do |issue|
+      @issues << {
+        id: issues.id,
+        title: issue.title,
+        price: issue.price,
+        user: {
+          id: issue.user_id,
+          name: issue.user.name
+        }
+      }
+    end
+
   end
 end
