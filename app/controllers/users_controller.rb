68b037b802c19793ce6bafd12c4a2dcc9017cfd3
tk1to <tk02dr@gmail.com>
2017-11-26 03:57:58 +0900

    access token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,21 @@ class UsersController < ApplicationController
 
   def create
     uri = URI.parse("https://api.github.com/user?access_token=#{params[:token]}")
+    require 'net/http'
     json = Net::HTTP.get(uri)
+    json = JSON.parse(json)
+    github_user_id = json['login']
+    if User.find_by(github_user_id: github_user_id)
+      render json: {
+        message: "already exist",
+        access_token: SecureRandom.urlsafe_base64
+      }
+    else
+      User.create(name: github_user_id, github_user_id: github_user_id)
+      render json: {
+        message: "created",
+        access_token: SecureRandom.urlsafe_base64
+      }
+    end
   end
 end
