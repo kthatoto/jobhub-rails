d801e307d013aea31d1cd5a7a6c61f0cb32a8bb2
tk1to <tk02dr@gmail.com>
2017-11-26 13:12:42 +0900

    payjp -> stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,5 +2,5 @@ Rails.configuration.stripe = {
   public_key: "pk_test_mVdMUbe6Ubosw57vl3K22ItG",
   secret_key: "sk_test_aFAEdBoAaWLtaKH7Cv0PBEW3",
 }
-Payjp.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
